b39e73268258acf96aa61b8186a33d8fa6ac474f
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-14 16:48:51 +0530

    Add files via upload

---
+++ list Python program to remove Nth occurrence of the given word.py
@@ -0,0 +1,14 @@
+# Programme by parth.py
+# Python program to remove Nth occurrence of the given word
+
+def fun(n):
+    lst = ["parth","patel","number"]
+    lst.pop(n)
+    print(lst)
+
+fun(0)
+
+"""
+output:
+['patel','number']
+"""
