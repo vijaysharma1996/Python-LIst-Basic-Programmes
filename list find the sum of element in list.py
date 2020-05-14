46059df6ae74561e6903baf0b35599a97465156b
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-14 20:28:32 +0530

    Add files via upload

---
+++ list find the sum of element in list.py
@@ -0,0 +1,16 @@
+# Programme by parth.py
+# Find the sum of elements in list
+
+def sum_ele(lst):
+    sum = 0
+    for i in lst:
+        sum = sum + i
+    print(f'Sum of lst elements-:{sum}')
+
+
+sum_ele([2,3,5,2,5,2,44,3,2,5,22,224,543,5543,4656546,6456456,3456547562,45645645456,2464645])
+
+"""
+output:
+Sum of lst elements-:19
+"""
