cd96000d4ab08a682b74dbca4ed209607d9d9bd5
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-14 17:02:15 +0530

    Add files via upload

---
+++ list Python  Ways to check if element exists in list.py
@@ -0,0 +1,19 @@
+# Programme by parth.py
+# Python  Ways to check if element exists in list
+def check(n,lst):
+    if n in lst:
+        print("Elements are available")
+    else:
+        print("Elements are not available")
+
+check(9,[1,2,3,4,5,6,3])
+
+str1 = 'parth patel'
+str = str1.split(" ")
+a = '-'.join(str)
+print(a)
+
+"""
+output:
+Element are not available
+"""
