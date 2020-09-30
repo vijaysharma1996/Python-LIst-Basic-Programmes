b7a191cd17c7b3c0f7f143ad75e73ca5027f1908
vijaysharma1996 <50214237+vijaysharma1996@users.noreply.github.com>
2020-09-30 12:54:41 -0700

    Update list find the sum of element in list.py

--- list find the sum of element in list.py
+++ list find the sum of element in list.py
@@ -1,16 +1,10 @@
-# Programme by parth.py
-# Find the sum of elements in list
+# Python program to find sum of elements in list 
 
-def sum_ele(lst):
-    sum = 0
-    for i in lst:
-        sum = sum + i
-    print(f'Sum of lst elements-:{sum}')
+# creating a list 
+list1 = [11, 5, 17, 18, 23] 
 
+# using sum() function 
+total = sum(list1) 
 
-sum_ele([2,3,5,2,5,2,44,3,2,5,22,224,543,5543,4656546,6456456,3456547562,45645645456,2464645])
-
-"""
-output:
-Sum of lst elements-:19
-"""
+# printing total value 
+print("Sum of all elements in given list: ", total) 
